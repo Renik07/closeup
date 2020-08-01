595d55d75524ee9b3c22c916d46a7fd23855921d
Renat Gayazutdinov <restspread@gmail.com>
2020-08-02 01:48:36 +0300

    добавил слайдер, библиотеку slick

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 $(function() {
-
+  $('.slider__inner').slick({
+    prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+    nextArrow: '<<button type="button" class="slick-btn slick-next"></button>',
+    infinite: false
+  });
 });
