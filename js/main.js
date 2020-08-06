d74dfafb2805c73343c7e1e7653f1839b57b1e34
Renat Gayazutdinov <restspread@gmail.com>
2020-08-06 22:57:17 +0300

    сверстал Новости (news)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(function() {
-  $('.slider__inner').slick({
+  $('.slider__inner, .news__slider-inner').slick({
     prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
     nextArrow: '<<button type="button" class="slick-btn slick-next"></button>',
     infinite: false
