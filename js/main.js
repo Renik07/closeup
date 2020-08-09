82ed6a560b5c886d24c01283e7c29a1d27811b76
Renat Gayazutdinov <restspread@gmail.com>
2020-08-09 20:32:07 +0300

    исправил недочеты

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(function() {
   $('.slider__inner, .news__slider-inner').slick({
     prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
-    nextArrow: '<<button type="button" class="slick-btn slick-next"></button>',
+    nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
     infinite: false
   });
 
